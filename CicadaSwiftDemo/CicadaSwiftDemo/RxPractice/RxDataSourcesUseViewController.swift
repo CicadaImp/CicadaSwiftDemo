697395739e39b9efc65d2d47e1b121c2cd6ebac2
Cicada01 <cicadav@126.com>
2022-06-18 17:28:50 +0800

    RxDataSources Use练习

---
+++ CicadaSwiftDemo/CicadaSwiftDemo/RxPractice/RxDataSourcesUseViewController.swift
@@ -0,0 +1,219 @@
+//
+//  RxDataSourcesUseViewController.swift
+//  CicadaSwiftDemo
+//
+//  Created by 知了 on 2022/6/18.
+//
+
+import UIKit
+import RxSwift
+import RxDataSources
+
+class RxDataSourcesUseViewController: BaseViewController {
+
+    var tableView: UITableView!
+    let reuseIdentfier = "myTableViewCell"
+    let disposeBag = DisposeBag()
+    let datas = GithubData()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView = UITableView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: 500), style: .grouped)
+        tableView.register(SectionTableViewCell.self, forCellReuseIdentifier: reuseIdentfier)
+        tableView.sectionFooterHeight = 0
+        view.addSubview(tableView)
+        
+        let dataSource = RxTableViewSectionedReloadDataSource<SectionModel<String, SectionDataModel>> { dataSource, tableView, indexPath, model in
+            let cell = tableView.dequeueReusableCell(withIdentifier: self.reuseIdentfier, for: indexPath) as? SectionTableViewCell
+            cell?.imageView?.image = model.image
+            cell?.textLabel?.text = model.name
+            cell?.detailTextLabel?.text = model.gitHubID
+            
+            return cell!
+        } titleForHeaderInSection: { dataSource, index in
+            return dataSource.sectionModels[index].model
+        }
+        
+        datas.githubData.asDriver(onErrorJustReturn: []).drive(tableView.rx.items(dataSource: dataSource)).disposed(by: disposeBag)
+        
+
+        tableView.rx.itemSelected.subscribe(onNext: {
+            [weak self] indexPath in
+            self?.navigationController?.pushViewController(HomeViewController(), animated: true)
+        }).disposed(by: disposeBag)
+
+        tableView.rx.setDelegate(self).disposed(by: disposeBag)
+    }
+    
+    
+    
+    
+
+}
+
+class SectionTableViewCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+struct SectionDataModel {
+    let name: String
+    let gitHubID: String
+    var image: UIImage?
+    init(name: String, gitHubID: String) {
+        self.name = name
+        self.gitHubID = gitHubID
+        image = UIImage(named: gitHubID)
+    }
+}
+
+//使用RxDataSources的唯一限制是，section中使用的每个类型都必须符合IdentifiableType和Equatable协议, IdentifiableType声明一个唯一的标识符（在同一具体类型的对象中是唯一的）
+extension SectionDataModel: IdentifiableType {
+    typealias Identity = String
+    var identity: Identity {
+        return gitHubID
+    }
+}
+
+class GithubData {
+    let githubData = Observable.just([
+            SectionModel(model: "A", items: [
+                SectionDataModel(name: "Alex V Bush", gitHubID: "alexvbush"),
+                SectionDataModel(name: "Andrew Breckenridge", gitHubID: "AndrewSB"),
+                SectionDataModel(name: "Anton Efimenko", gitHubID: "reloni"),
+                SectionDataModel(name: "Ash Furrow", gitHubID: "ashfurrow"),
+                ]),
+            SectionModel(model: "B", items: [
+                SectionDataModel(name: "Ben Emdon", gitHubID: "BenEmdon"),
+                SectionDataModel(name: "Bob Spryn", gitHubID: "sprynmr"),
+                ]),
+            SectionModel(model: "C", items: [
+                SectionDataModel(name: "Carlos García", gitHubID: "carlosypunto"),
+                SectionDataModel(name: "Cezary Kopacz", gitHubID: "CezaryKopacz"),
+                SectionDataModel(name: "Chris Jimenez", gitHubID: "PiXeL16"),
+                SectionDataModel(name: "Christian Tietze", gitHubID: "DivineDominion"),
+                ]),
+            SectionModel(model: "D", items: [
+                SectionDataModel(name: "だいちろ", gitHubID: "mokumoku"),
+                SectionDataModel(name: "David Alejandro", gitHubID: "davidlondono"),
+                SectionDataModel(name: "David Paschich", gitHubID: "dpassage"),
+                ]),
+            SectionModel(model: "E", items: [
+                SectionDataModel(name: "Esteban Torres", gitHubID: "esttorhe"),
+                SectionDataModel(name: "Evgeny Aleksandrov", gitHubID: "ealeksandrov"),
+                ]),
+            SectionModel(model: "F", items: [
+                SectionDataModel(name: "Florent Pillet", gitHubID: "fpillet"),
+                SectionDataModel(name: "Francis Chong", gitHubID: "siuying"),
+                ]),
+            SectionModel(model: "G", items: [
+                SectionDataModel(name: "Giorgos Tsiapaliokas", gitHubID: "terietor"),
+                SectionDataModel(name: "Guy Kahlon", gitHubID: "GuyKahlon"),
+                SectionDataModel(name: "Gwendal Roué", gitHubID: "groue"),
+                ]),
+            SectionModel(model: "H", items: [
+                SectionDataModel(name: "Hiroshi Kimura", gitHubID: "muukii"),
+                ]),
+            SectionModel(model: "I", items: [
+                SectionDataModel(name: "Ivan Bruel", gitHubID: "ivanbruel"),
+                ]),
+            SectionModel(model: "J", items: [
+                SectionDataModel(name: "Jeon Suyeol", gitHubID: "devxoul"),
+                SectionDataModel(name: "Jérôme Alves", gitHubID: "jegnux"),
+                SectionDataModel(name: "Jesse Farless", gitHubID: "solidcell"),
+                SectionDataModel(name: "Junior B.", gitHubID: "bontoJR"),
+                SectionDataModel(name: "Justin Swart", gitHubID: "justinswart"),
+                ]),
+            SectionModel(model: "K", items: [
+                SectionDataModel(name: "Karlo", gitHubID: "floskel"),
+                SectionDataModel(name: "Krunoslav Zaher", gitHubID: "kzaher"),
+                ]),
+            SectionModel(model: "L", items: [
+                SectionDataModel(name: "Laurin Brandner", gitHubID: "lbrndnr"),
+                SectionDataModel(name: "Lee Sun-Hyoup", gitHubID: "kciter"),
+                SectionDataModel(name: "Leo Picado", gitHubID: "leopic"),
+                SectionDataModel(name: "Libor Huspenina", gitHubID: "libec"),
+                SectionDataModel(name: "Lukas Lipka", gitHubID: "lipka"),
+                SectionDataModel(name: "Łukasz Mróz", gitHubID: "sunshinejr"),
+                ]),
+            SectionModel(model: "M", items: [
+                SectionDataModel(name: "Marin Todorov", gitHubID: "icanzilb"),
+                SectionDataModel(name: "Maurício Hanika", gitHubID: "mAu888"),
+                SectionDataModel(name: "Maximilian Alexander", gitHubID: "mbalex99"),
+                ]),
+            SectionModel(model: "N", items: [
+                SectionDataModel(name: "Nathan Kot", gitHubID: "nathankot"),
+                ]),
+            SectionModel(model: "O", items: [
+                SectionDataModel(name: "Orakaro", gitHubID: "DTVD"),
+                SectionDataModel(name: "Orta", gitHubID: "orta"),
+                ]),
+            SectionModel(model: "P", items: [
+                SectionDataModel(name: "Paweł Urbanek", gitHubID: "pawurb"),
+                SectionDataModel(name: "Pedro Piñera Buendía", gitHubID: "pepibumur"),
+                SectionDataModel(name: "PG Herveou", gitHubID: "pgherveou"),
+                ]),
+            SectionModel(model: "R", items: [
+                SectionDataModel(name: "Rui Costa", gitHubID: "ruipfcosta"),
+                SectionDataModel(name: "Ryo Fukuda", gitHubID: "yuzushioh"),
+                ]),
+            SectionModel(model: "S", items: [
+                SectionDataModel(name: "Scott Gardner", gitHubID: "scotteg"),
+                SectionDataModel(name: "Scott Hoyt", gitHubID: "scottrhoyt"),
+                SectionDataModel(name: "Sendy Halim", gitHubID: "sendyhalim"),
+                SectionDataModel(name: "Serg Dort", gitHubID: "sergdort"),
+                SectionDataModel(name: "Shai Mishali", gitHubID: "freak4pc"),
+                SectionDataModel(name: "Shams Ahmed", gitHubID: "shams-ahmed"),
+                SectionDataModel(name: "Shenghan Chen", gitHubID: "zzdjk6"),
+                SectionDataModel(name: "Shunki Tan", gitHubID: "milkit"),
+                SectionDataModel(name: "Spiros Gerokostas", gitHubID: "sger"),
+                SectionDataModel(name: "Stefano Mondino", gitHubID: "stefanomondino"),
+                ]),
+            SectionModel(model: "T", items: [
+                SectionDataModel(name: "Thane Gill", gitHubID: "thanegill"),
+                SectionDataModel(name: "Thomas Duplomb", gitHubID: "tomahh"),
+                SectionDataModel(name: "Tomasz Pikć", gitHubID: "pikciu"),
+                SectionDataModel(name: "Tony Arnold", gitHubID: "tonyarnold"),
+                SectionDataModel(name: "Torsten Curdt", gitHubID: "tcurdt"),
+                ]),
+            SectionModel(model: "V", items: [
+                SectionDataModel(name: "Vladimir Burdukov", gitHubID: "chipp"),
+                ]),
+            SectionModel(model: "W", items: [
+                SectionDataModel(name: "Wolfgang Lutz", gitHubID: "Lutzifer"),
+                ]),
+            SectionModel(model: "X", items: [
+                SectionDataModel(name: "xixi197 Nova", gitHubID: "xixi197"),
+                ]),
+            SectionModel(model: "Y", items: [
+                SectionDataModel(name: "Yongha Yoo", gitHubID: "inkyfox"),
+                SectionDataModel(name: "Yosuke Ishikawa", gitHubID: "ishkawa"),
+                SectionDataModel(name: "Yury Korolev", gitHubID: "yury"),
+                ]),
+            ])
+    
+}
+
+extension RxDataSourcesUseViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 30
+    }
+    
+    //同时实现以下两个方法，footer设置才有效（仅设置 tableView.sectionFooterHeight = 0 也有效）
+//    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+//        return 0.001
+//    }
+//
+//    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+//        return nil
+//    }
+}
--- CicadaSwiftDemo/Pods/Target Support Files/Pods-CicadaSwiftDemo/Pods-CicadaSwiftDemo-frameworks.sh
+++ CicadaSwiftDemo/Pods/Target Support Files/Pods-CicadaSwiftDemo/Pods-CicadaSwiftDemo-frameworks.sh
@@ -177,11 +177,13 @@ code_sign_if_enabled() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Differentiator/Differentiator.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Dollar/Dollar.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Kingfisher/Kingfisher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MonkeyKing/MonkeyKing.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/R.swift.Library/Rswift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxDataSources/RxDataSources.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxRelay/RxRelay.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
@@ -189,11 +191,13 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Differentiator/Differentiator.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Dollar/Dollar.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Kingfisher/Kingfisher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MonkeyKing/MonkeyKing.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/R.swift.Library/Rswift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxDataSources/RxDataSources.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxRelay/RxRelay.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
